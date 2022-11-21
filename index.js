0b7cafc86f75f5c7b47ace464a89b096aef8ee93
Shubham-techuz <shubham@techuz.com>
2022-11-21 12:13:43 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require("cors");
+require('./db/config');
+const CardDetail = require('./schemas/cardDetail');
+const CompleteTime = require('./schemas/completeTime');
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 5000
+
+app.use(express.json());
+app.use(cors());
+
+
+// Getting the complete Timestamp 
+app.get("/timestamp/:id", async (req,res) => {
+    try {
+        let result = await CompleteTime.findOne({cardId: req.params.id});
+        if(result){
+            res.send(result);
+        }else{
+            res.send({result: "No Timestamp found"});
+        }
+    } catch(err) {
+        console.log("Get Timestamp Error-----> ",err);
+    }
+});
+
+// Made changes to TimeStamp
+app.put("/timestamp/:id", async (req,res) => {
+    try {
+        let result = await CompleteTime.updateOne(
+            {cardId: req.params.id},
+            {
+                 $set: req.body
+            }
+        )
+        res.send(result);
+    } catch(err) {
+        console.log("Put Timestamp Error-----> ",err);
+    }
+});
+
+// Storing the Timestamp
+app.post("/timestamp", async (req,res) => {
+    try {
+        let completeTime = new CompleteTime(req.body);
+        let result = await completeTime.save();
+        res.send(result);
+    } catch(err) {
+        console.log("Post Timestamp Error-----> ",err);
+    }
+});
+
+// Storing Card Detail
+app.post("/", async (req,res) => {
+    try {
+        let card = new CardDetail(req.body);
+        let result = await card.save();
+        res.send(result);
+
+    } catch(err) {
+        console.log("Error-----> ",err);
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+});
+
---
+++ schemas/completeTime.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const completeTimeSchema = new mongoose.Schema({
+    cardId: String,
+    completeTime: String
+});
+
+module.exports = mongoose.model('completeTime', completeTimeSchema);
