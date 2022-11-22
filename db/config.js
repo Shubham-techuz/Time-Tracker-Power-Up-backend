b2a4eb610808003180517b9478b1435bfc4ebfef
Shubham-techuz <shubham@techuz.com>
2022-11-22 17:56:44 +0530

    .env file and config file changes

--- db/config.js
+++ db/config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://shubham:root@cluster0.jridc0z.mongodb.net/time-tracker-power-up?retryWrites=true&w=majority');
+mongoose.connect(process.env.URL);
